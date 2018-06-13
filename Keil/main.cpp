92124924dfcea7b9a0e954e50f8b56a8b7941469
wy56 <zxc41329@gmail.com>
2018-06-13 16:54:35 +0800

    Smart-Clock
    Clock Reset Zero

--- Keil/main.cpp
+++ Keil/main.cpp
@@ -2,63 +2,89 @@
 #include "Sht31.h"
 #include "pinmap.h"
 
+AnalogIn analogValue1(A1);
+AnalogIn analogValue2(A2);
+DigitalOut out_min(PA_4);
+DigitalOut out_hour(PA_5);
+
 Sht31 sht31 = Sht31(I2C_SDA, I2C_SCL);
 DigitalOut LED(PB_0); 
+
 Serial pc(USBTX, USBRX); 
 
-void initialization(void);
-void grafect(void);
-void action(void);
-void printState(void);
+void hour_click(void);
+void min_click(void);
+void reset_zero(void);
 
-int Input, X1, X2, X3, X4;
-float temp, humid;
+float v_hour, v_min, v_out1, v_out2;
+int now_hour, now_min;
 
 int main()
 {
-		initialization();
-    while (true) {
-			grafect();
-			wait(1);
-    }	
-}
+		reset_zero();
+		
 
-void initialization() {
-		Input=1, X1=0, X2=0, X3=0, X4=0;
-}
+		/*while(1) {
+			v_hour = analogValue1 * 33.f;
+			v_min = analogValue2  * 33.f;
+			
+			printf("v_hour = %0.2fV, v_min = %0.2fV.\r\n", v_hour, v_min);
+			
+			
 
-void grafect() {
-		if(Input == 1) Input=0, X1=1;
-		else if(X1 == 1) X1=0, X2=1;
-		else if(X2 == 1 && ((int)temp > 28 || (int)humid > 60)) X2=0, X3=1;
-		else if(X2 == 1 && (int)temp <= 28 && (int)humid <= 60) X2=0, X4=1;
-		else if(X3 == 1) X3=0, Input=1;
-	  else if(X4 == 1) X4=0, Input=1;
-		action();
-		printState();
-}
-
-void printState() {
-		pc.printf("Input = %d, X1 = %d, X2 = %d, X3 = %d, X4 = %d\r\n", Input, X1, X2, X3, X4);
-}
-
-void action() {
-		if(Input == 1) {
-			temp = sht31.readTemperature();
-		}
-		else if(X1 == 1) {
-			humid = sht31.readHumidity();
-			pc.printf("~ Result of STH31-D Sensor ~\r\n");
-			pc.printf("-------------------------------\r\n");
-			pc.printf("Temperature : %.2f oC\r\n", temp);
-			pc.printf("Humidity    : %.2f %%\r\n\r\n\r\n", humid);
-		}
-		else if(X2 == 1 && ((int)temp > 28 || (int)humid > 60)) {
+			
+			
+			wait(0.2);
 			LED = 1;
-			pc.printf("LED is on\r\n");
-		}
-		else if(X2 == 1 && (int)temp <= 28 && (int)humid <= 60) {
+			out1=1;
+			wait(0.07);
 			LED = 0;
-			pc.printf("LED is off\r\n");
-		}
+			out1=0;
+			wait(0.08);
+			LED = 1;
+			out2=1;
+			wait(0.07);
+			LED = 0;
+			out2=0;
+			wait(0.08);
+			
+			
+		}*/
 }
+
+void hour_click() {
+	out_hour = 1;
+	wait(0.05);
+	out_hour = 0;
+	wait(0.1);
+}
+
+void min_click() {
+	out_min = 1;
+	wait(0.05);
+	out_min = 0;
+	wait(0.1);
+}
+
+void reset_zero() {
+	while(v_hour<30) {
+		v_hour = analogValue1 * 33.f;
+		hour_click();
+	}
+	
+	while(v_hour>30) {
+		hour_click();
+		v_hour = analogValue1  * 33.f;	
+	}
+
+	while(v_min<30) {
+		v_min = analogValue2  * 33.f;
+		min_click();
+	}
+	
+	while(v_min>30) {
+		min_click();
+		v_min = analogValue2  * 33.f;
+	}
+}
+
