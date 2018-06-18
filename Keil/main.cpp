077124e5874ccc6c88926213c96b9896360a9517
wy56 <zxc41329@gmail.com>
2018-06-18 18:20:50 +0800

    Smart-Clock

--- Keil/main.cpp
+++ Keil/main.cpp
@@ -6,23 +6,27 @@ AnalogIn analogValue1(A1);
 AnalogIn analogValue2(A2);
 DigitalOut out_min(PA_4);
 DigitalOut out_hour(PA_5);
-
 Sht31 sht31 = Sht31(I2C_SDA, I2C_SCL);
-DigitalOut LED(PB_0); 
-
 Serial pc(USBTX, USBRX); 
 
+int i, v_out1, v_out2;
+float v_hour, v_min;
+int now_hour = 19, now_min = 41;
+double now_sec = 12;
+
+void wait_clock(double);
 void hour_click(void);
 void min_click(void);
-void reset_zero(void);
-
-float v_hour, v_min, v_out1, v_out2;
-int now_hour, now_min;
+void reset_clock(void);
+void set_clock(int, int);
 
 int main()
 {
-		reset_zero();
+		set_clock(now_hour, now_min);
 		
+		while(1) {
+			wait_clock(60.f - now_sec);
+		}
 
 		/*while(1) {
 			v_hour = analogValue1 * 33.f;
@@ -30,43 +34,43 @@ int main()
 			
 			printf("v_hour = %0.2fV, v_min = %0.2fV.\r\n", v_hour, v_min);
 			
-			
+		*/
+}
 
-			
-			
-			wait(0.2);
-			LED = 1;
-			out1=1;
-			wait(0.07);
-			LED = 0;
-			out1=0;
-			wait(0.08);
-			LED = 1;
-			out2=1;
-			wait(0.07);
-			LED = 0;
-			out2=0;
-			wait(0.08);
-			
-			
-		}*/
+void wait_clock(double sec) {
+	wait(sec);
+	now_sec += sec;
+	if(now_sec >= 60.f) {
+		now_sec -= 60;
+		now_min += 1;
+		min_click();
+		if(now_min >= 60) {
+			hour_click();
+			hour_click();
+			now_min -= 60;
+			now_hour = (now_hour + 1) % 24;
+			if(now_hour >= 12) {
+				hour_click();
+			}
+		}
+	}
 }
 
 void hour_click() {
 	out_hour = 1;
-	wait(0.05);
+	wait_clock(0.05);
 	out_hour = 0;
-	wait(0.1);
+	wait_clock(0.1);
 }
 
 void min_click() {
 	out_min = 1;
-	wait(0.05);
+	wait_clock(0.05);
 	out_min = 0;
-	wait(0.1);
+	wait_clock(0.1);
 }
 
-void reset_zero() {
+void reset_clock() {
 	while(v_hour<30) {
 		v_hour = analogValue1 * 33.f;
 		hour_click();
@@ -88,3 +92,16 @@ void reset_zero() {
 	}
 }
 
+void set_clock(int now_hour, int now_min) {
+	reset_clock();
+	for(i=0; i<now_hour; i++) {
+		if(i >= 12) {
+			hour_click();
+		}
+		hour_click();
+		hour_click();
+	}
+	for(i=0; i<now_min; i++) {
+		min_click();
+	}
+}
